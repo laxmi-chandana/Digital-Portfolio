62527bb8d9b2967aad9d616b9daf188f996feba4
laxmi-chandana <36924060+laxmi-chandana@users.noreply.github.com>
2021-01-11 19:28:19 +0530

    Digital Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,125 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/laxmi-chandana",
+    "name": "Lakshmi Avvaru",
+    "role": "Software Engineer and Web Developer",
+    "linkedinId":"Lakshmi Avvaru",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Currently pursuing Masters in Computer Science at California State University ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/lakshmi-avvaru/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/laxmi-chandana",
+          "className":"fa fa-github"
+        },
+
+      ],
+    "aboutme":"Effective collaborator who works well with multiple teams and is adept at working within ambiguous environments and with undefined challenges through strong problem-solving skills and effective communication. Proficient in multiple programming languages and has vast experience with various tools and technologies and is eager to learn more within an Internship.",
+    "address":"California",
+    "phone":"+1(559)394-1249",
+    "email":"alaxmichandana@gmail.com",
+    "education":[
+      {
+        "UniversityName":"California State University, Fresno",
+        "specialization":"Computer Science",
+        "StartDate":"August 2019 - ",
+        "EndDate":"May 2021",
+        "Achievements":"Related Course Work - Design Analysis and Algorithms, Advanced DataBase Management System.",
+      },
+      {
+        "UniversityName":"Jawaharlal Nehru Technological University",
+        "specialization":"Computer Science",
+        "StartDate":"August 2014 - ",
+        "EndDate":"May 2018",
+        "Achievements":"Related Course Work - Data Structures, Web Technologies."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Tata Consultancy Services",
+        "specialization":"System Engineer",
+        "StartDate":"June 2018 - ",
+        "EndDate":"June 2019",
+        "Achievements":"Implementing MVC architecture for web pages of infotainment system. • For developing the View part, we used HTML,CSS. • Performed unit testing and Pre-Integration testing for project. • Scrum-based development methodology that includes daily stand-ups, regular sprint review, and retrospectives to discuss work with a cross-functional team using agile development processes. • Experience in communicating with users, other technical teams, and senior management to collect equirements, describe software product features, technical designs, and product strategy",
+      },
+      {
+        "CompanyName":"Office of Community & Economic Development at Fresno State",
+        "specialization":"Census Worker",
+        "StartDate":"March 2020 -",
+        "EndDate":"September 2020",
+        "Achievements":"Canvass for the Census and also running our Questionnaire Assistance Centers and Kiosks organizing. The locations targeted the communities in and around the CSU Fresno area and the Pinedale Neighborhood."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"C"
+      },
+      {
+        "skillname":"python"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JSON"
+      },
+      {
+        "skillname":"MYSQL"
+      }
+    ],
+    "portfolio":[
+
+      {
+        "name":"Project - Auth-App",
+        "description":"•	Implemented MEAN stack – ability to work with Angular, Node.JS, Express.JS, MongoDB, where the backend consists of Node and the frontend consists of Angular",
+        "imgurl":"images/auth-app.png",
+        "linkurl" : "https://github.com/laxmi-chandana/Mini-Facebook"
+      },
+      {
+        "name":"Project - RESORT",
+        "description":"•	Developed a website where a customer can have a overlook of different types of rooms in a beach side resort and information about all those rooms by filtering according to type, size, price, etc. Utilized Reactjs",
+        "imgurl":"images/resort.png",
+        "linkurl" : "https://github.com/laxmi-chandana/ReactJS-Resort-Project"
+      },
+        {
+        "name":"Project - SwagShopAPI",
+        "description":"•Designed and Implemented the RESTful APIs that the user can fetch products, post products and even populate the data",
+        "imgurl":"images/SwagShop-API.png",
+        "linkurl" : "https://github.com/laxmi-chandana/Swag-Shop-API"
+      },{
+        "name":"Project - BudgetCalculator",
+        "description":"•	Developed a react-based calculator where a user can add and delete all his expenses.",
+        "imgurl":"images/BudgetCalculator.png",
+        "linkurl" : "https://github.com/laxmi-chandana/BudgetCalculator"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
